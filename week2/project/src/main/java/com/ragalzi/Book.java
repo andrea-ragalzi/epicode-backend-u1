01de67b047c771d63734cc10614fed681390eb00
andrea-ragalzi <andrea.ragalzi.code@gmail.com>
2023-05-15 01:39:00 +0200

    new constructor with lines from txt file

--- week2/project/src/main/java/com/ragalzi/Book.java
+++ week2/project/src/main/java/com/ragalzi/Book.java
@@ -19,8 +19,31 @@ public class Book extends Publication {
         this.genre = genre;
     }
 
+    public Book(List<String> lines) throws IllegalArgumentException, IndexOutOfBoundsException {
+        super();
+        if (!lines.get(0).equals("Book{")) {
+            throw new IllegalArgumentException("Invalid line format for Book: " + lines);
+        }
+        String isbn = lines.get(1).split("=")[1].replaceAll(",", "").replaceAll("'", "");
+        String title = lines.get(2).split("=")[1].replaceAll(",", "").replaceAll("'", "");
+        int year = Integer.parseInt(lines.get(3).split("=")[1].replaceAll(",", "").replaceAll("'", ""));
+        int numPages = Integer.parseInt(lines.get(4).split("=")[1].replaceAll(",", "").replaceAll("'", ""));
+        String author = lines.get(5).split("=")[1].replaceAll(",", "").replaceAll("'", "");
+        String genre = lines.get(6).split("=")[1].replaceAll(",", "").replaceAll("'", "");
+        this.setIsbn(isbn);
+        this.setTitle(title);
+        this.setYear(year);
+        this.setNumPages(numPages);
+        this.setAuthor(author);
+        this.setGenre(genre);
+    }
+
     public String getAuthor() {
         return this.author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
     }
 
     public String getGenre() {
@@ -41,19 +64,6 @@ public class Book extends Publication {
                 ",\n \tauthor='" + this.author + '\'' +
                 ",\n \tgenre='" + this.genre + '\'' +
                 "\n}";
-    }
-
-    public static Book createFromLines(List<String> lines) throws IllegalArgumentException, IndexOutOfBoundsException {
-        if (!lines.get(0).equals("Book{")) {
-            throw new IllegalArgumentException("Invalid line format for Book: " + lines);
-        }
-        String isbn = lines.get(1).split("=")[1].replaceAll(",", "").replaceAll("'", "");
-        String title = lines.get(2).split("=")[1].replaceAll(",", "").replaceAll("'", "");
-        int year = Integer.parseInt(lines.get(3).split("=")[1].replaceAll(",", "").replaceAll("'", ""));
-        int numPages = Integer.parseInt(lines.get(4).split("=")[1].replaceAll(",", "").replaceAll("'", ""));
-        String author = lines.get(5).split("=")[1].replaceAll(",", "").replaceAll("'", "");
-        String genre = lines.get(6).split("=")[1].replaceAll(",", "").replaceAll("'", "");
-        return new Book(isbn, title, year, numPages, author, genre);
     }
 
 }
--- week2/project/src/main/java/com/ragalzi/Magazine.java
+++ week2/project/src/main/java/com/ragalzi/Magazine.java
@@ -10,17 +10,29 @@ public class Magazine extends Publication {
         this.periodicity = periodicity;
     }
 
-    public Magazine(String isbn, String title, int year, int numPages, Periodicity periodicity) {
-        super(title, year, numPages);
-        this.isbn = isbn;
-        this.periodicity = periodicity;
+    public Magazine(List<String> lines) throws IllegalArgumentException, IndexOutOfBoundsException {
+        super();
+        if (!lines.get(0).equals("Magazine{")) {
+            throw new IllegalArgumentException("Invalid line format for Magazine: " + lines);
+        }
+        String isbn = lines.get(1).split("=")[1].replaceAll(",", "").replaceAll("'", "");
+        String title = lines.get(2).split("=")[1].replaceAll(",", "").replaceAll("'", "");
+        int year = Integer.parseInt(lines.get(3).split("=")[1].replaceAll(",", "").replaceAll("'", ""));
+        int numPages = Integer.parseInt(lines.get(4).split("=")[1].replaceAll(",", "").replaceAll("'", ""));
+        Periodicity periodicity = Periodicity
+                .valueOf(lines.get(5).split("=")[1].replaceAll(",", "").replaceAll("'", ""));
+        this.setIsbn(isbn);
+        this.setTitle(title);
+        this.setYear(year);
+        this.setNumPages(numPages);
+        this.setPeriodicity(periodicity);
     }
 
     public Periodicity getPeriodicity() {
         return this.periodicity;
     }
 
-    public void setPeriodicity(Periodicity periodicity) {
+    public void setPeriodicity(Periodicity periodicity) throws IllegalArgumentException {
         this.periodicity = periodicity;
     }
 
@@ -33,20 +45,6 @@ public class Magazine extends Publication {
                 ",\n\tnumPages=" + this.getNumPages() +
                 ",\n\tperiodicity='" + this.periodicity + '\'' +
                 "\n}";
-    }
-
-    public static Magazine createFromLines(
-            List<String> lines) throws IllegalArgumentException, IndexOutOfBoundsException {
-        if (!lines.get(0).equals("Magazine{")) {
-            throw new IllegalArgumentException("Invalid line format for Magazine: " + lines);
-        }
-        String isbn = lines.get(1).split("=")[1].replaceAll(",", "").replaceAll("'", "");
-        String title = lines.get(2).split("=")[1].replaceAll(",", "").replaceAll("'", "");
-        int year = Integer.parseInt(lines.get(3).split("=")[1].replaceAll(",", "").replaceAll("'", ""));
-        int numPages = Integer.parseInt(lines.get(4).split("=")[1].replaceAll(",", "").replaceAll("'", ""));
-        Periodicity periodicity = Periodicity
-                .valueOf(lines.get(5).split("=")[1].replaceAll(",", "").replaceAll("'", ""));
-        return new Magazine(isbn, title, year, numPages, periodicity);
     }
 
 }
