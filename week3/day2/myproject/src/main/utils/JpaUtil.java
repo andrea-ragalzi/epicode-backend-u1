cd78047b98fb6dfe8162122b21791b1c35288752
andrea-ragalzi <andrea.ragalzi.code@gmail.com>
2023-05-17 09:38:43 +0200

    uploaded exercise backend day 2

--- week3/day2/myproject/src/main/utils/JpaUtil.java
+++ week3/day2/myproject/src/main/utils/JpaUtil.java
@@ -10,7 +10,7 @@ public class JpaUtil {
 	static {
 		try {
 			entityManagerFactory = Persistence
-					.createEntityManagerFactory("day3");
+					.createEntityManagerFactory("day2");
 		} catch (Throwable ex) {
 			System.err.println("Initial EntityManagerFactory creation failed."
 					+ ex);
