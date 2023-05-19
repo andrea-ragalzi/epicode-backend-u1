2bacbcaf44b7b6a348e63a7f961e53e101eda84e
andrea-ragalzi <andrea.ragalzi.code@gmail.com>
2023-05-19 23:48:47 +0200

    change endDate for loan

--- week3/project/src/main/java/com/ragalzi/App.java
+++ week3/project/src/main/java/com/ragalzi/App.java
@@ -33,35 +33,28 @@ public final class App {
                 publicationDAO.save(magazine);
 
                 // Rimozione di un elemento del catalogo dato un codice ISBN
-                System.out.println(
-                                String.format(
-                                                "Publication removed by ISBN: %s\n\n %s",
-                                                book.getIsbn(), book));
+                System.out.println(String.format(
+                                "Publication removed by ISBN: %s\n\n %s",
+                                book.getIsbn(), book));
                 publicationDAO.removeByIsbn(book.getIsbn());
 
                 // Ricerca per ISBN
                 Publication publicationFound = publicationDAO.getByIsbn("00008");
-                System.out.println(
-                                String.format(
-                                                "Publication found by ISBN: %s\n\n %s",
-                                                publicationFound.getIsbn(), publicationFound));
+                System.out.println(String.format(
+                                "Publication found by ISBN: %s\n\n %s",
+                                publicationFound.getIsbn(), publicationFound));
 
                 // Ricerca per anno pubblicazione
                 int year = 2017;
                 List<Publication> publicationsByYear = publicationDAO.getByYear(year);
-                System.out.println(
-                                String.format(
-                                                "Publications found by year: %s\n\n %s",
-                                                year, publicationsByYear));
-
+                System.out.println(String.format("Publications found by year: %s\n\n %s",
+                                year, publicationsByYear));
                 // Ricerca per autore
                 String author = "George Orwell";
                 List<Publication> publicationsByAuthor = publicationDAO.getByAuthor(author);
-                System.out.println(
-                                String.format(
-                                                "Publications found by author: %s\n\n %s",
-                                                author, publicationsByAuthor));
-
+                System.out.println(String.format(
+                                "Publications found by author: %s\n\n %s",
+                                author, publicationsByAuthor));
                 // Ricerca per titolo o parte di esso
                 String searchTerm = "The Great Gatsby";
                 List<Publication> publicationsByTitle = publicationDAO.getByTitle(searchTerm);
@@ -71,14 +64,14 @@ public final class App {
                                                 searchTerm, publicationsByTitle));
 
                 // Generate and save random users
-                List<User> users = randomUsers(10);
+                List<User> users = randomUsers(5);
                 UserDAO userDAO = new UserDAO();
                 userDAO.saveAll(users);
 
+                LoanDAO loanDAO = new LoanDAO();
                 // Generate and save random loans
                 List<Loan> loans = randomLoans(10);
                 System.out.println(loans);
-                LoanDAO loanDAO = new LoanDAO();
                 loanDAO.saveAll(loans);
 
                 // Ricerca degli elementi attualmente in prestito dato un numero di tessera
@@ -94,14 +87,14 @@ public final class App {
                 List<Loan> loansNotReturned = loanDAO.getExpiredNotReturnedLoans();
                 System.out.println(
                                 String.format(
-                                                "Loans not returned:\n\n %s",
-                                                loansNotReturned));
+                                                "Loans not returned %s",
+                                                loansNotReturned.size()));
         }
 
         public static List<User> randomUsers(int quantity) {
                 System.out.println(
-                                String.format(
-                                                "Generating %d random users\n", quantity));
+                                String.format("Generating %d random users\n",
+                                                quantity));
                 List<User> users = new ArrayList<User>();
                 for (int i = 0; i < quantity; i++) {
                         users.add(User.randomUser());
@@ -110,27 +103,50 @@ public final class App {
         }
 
         public static List<Loan> randomLoans(int quantity) {
-                System.out.println(
-                                String.format(
-                                                "Generating %d random loans\n", quantity));
                 Random random = new Random();
                 UserDAO userDAO = new UserDAO();
-                List<User> users = new ArrayList<User>();
-                users = userDAO.getAll();
+                List<User> users = userDAO.getAll();
                 PublicationDAO publicationDAO = new PublicationDAO();
-                List<Publication> publications = new ArrayList<Publication>();
-                publications = publicationDAO.getAll();
-                List<Loan> loans = new ArrayList<Loan>();
+                List<Publication> publications = publicationDAO.getAll();
+                List<Loan> loans = new ArrayList<>();
+                LocalDate startDateMin = LocalDate.now().minusYears(10);
+                LocalDate startDateMax = LocalDate.now();
                 for (int i = 0; i < quantity; i++) {
                         User user = users.get(random.nextInt(users.size()));
-                        Publication publication = publications.get(random.nextInt(publications.size()));
-                        LocalDate startDate = LocalDate.now().minusDays(random.nextInt(30));
-                        LocalDate endDate = LocalDate.now().minusDays(random.nextInt(30));
-                        LocalDate returnDate = endDate.plusDays(random.nextInt(7));
-                        Loan loan = new Loan(user, publication, startDate, endDate, returnDate);
+                        Publication publication = publications.get(
+                                        random.nextInt(publications.size()));
+                        LocalDate startDate = getRandomStartDate(
+                                        startDateMin, startDateMax, random);
+                        LocalDate endDate = startDate.plusMonths(1);
+                        LocalDate returnDate = getRandomReturnDate(
+                                        startDate, endDate, i, random);
+                        Loan loan = new Loan(user, publication, startDate,
+                                        endDate, returnDate);
                         loans.add(loan);
                 }
                 return loans;
+        }
+
+        private static LocalDate getRandomStartDate(
+                        LocalDate startDateMin, LocalDate startDateMax,
+                        Random random) {
+                long daysBetween = startDateMax.toEpochDay() - startDateMin.toEpochDay();
+                long randomDays = (long) (random.nextDouble() * daysBetween);
+                return startDateMin.plusDays(randomDays);
+        }
+
+        private static LocalDate getRandomReturnDate(
+                        LocalDate startDate, LocalDate endDate,
+                        int index, Random random) {
+                LocalDate returnDate = null;
+                if (index % 3 == 0) {
+                        int randomDaysOffset = random.nextInt(30);
+                        returnDate = endDate.plusDays(randomDaysOffset);
+                } else if (index % 5 == 0) {
+                        int randomDaysOffset = random.nextInt(30);
+                        returnDate = startDate.minusDays(randomDaysOffset);
+                }
+                return returnDate;
         }
 
 }
